e5760025d94911c1265e1d1e06a900928b38ee6b
Halyna Rodriguez <loza1111@yahoo.com>
2021-09-12 12:56:15 -0400

    commit and push

--- Lab2/Lab2.php
+++ Lab2/Lab2.php
@@ -1,31 +1,57 @@
-<form>
-  <p>
-  Name<br>
-  <input type="input" id="name" name="name" placeholder="Name">
-  </p>
-  <p>
-  Major<br>
-  <input type="input" id="major" name="major" placeholder="Major">
-  </p>
-  <p>
-  Favorite Web Language<br>
-  <input type="radio" id="html" name="fav_language" value="HTML">
-  <label for="html">HTML</label><br>
-  <input type="radio" id="css" name="fav_language" value="CSS">
-  <label for="css">CSS</label><br>
-  <input type="radio" id="javascript" name="fav_language" value="JavaScript">
-  <label for="javascript">JavaScript</label><br>
-  <input type="radio" id="PHP" name="fav_language" value="PHP">
-  <label for="PHP">PHP</label>
-  </p>
-  <p>
-  Development Evironment<br>
-  <input type="checkbox" id="ide1" name="vscode" value="vscode">
-  <label for="vehicle1"> Visual Studio Code</label><br>
-  <input type="checkbox" id="ide2" name="brackets" value="brackets">
-  <label for="vehicle2"> Brackets</label><br>
-  <input type="checkbox" id="ide3" name="other" value="other">
-  <label for="vehicle3"> Other</label> 
-  <input type="text" id="othername" name="othername">
-  </p>
-</form> 
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Form</title>
+<style>
+        /* add this class for centralizing the content*/
+        .main{
+                margin: 0 auto;
+                width: 500px;
+                border: 1px solid blue;
+                padding: 30px;
+        }
+</style>
+</head>
+<body>
+<div class="main">
+        <!-- set form action page is to 'summary.php'-->
+        <form action="summary.php">
+          <p>
+          Name<br>
+          <input type="input" id="name" name="name" placeholder="Name">
+          </p>
+          <p>
+          Major<br>
+          <input type="input" id="major" name="major" placeholder="Major">
+          </p>
+          <p>
+          Favorite Web Language<br>
+          <input type="radio" id="html" name="fav_language" value="HTML">
+          <label for="html">HTML</label><br>
+          <input type="radio" id="css" name="fav_language" value="CSS">
+          <label for="css">CSS</label><br>
+          <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+          <label for="javascript">JavaScript</label><br>
+          <input type="radio" id="PHP" name="fav_language" value="PHP">
+          <label for="PHP">PHP</label>
+          </p>
+          <p>
+          Development Environment<br>
+          <!-- change checkboxs name to env and as treated as checkbox array-->
+          <input type="checkbox" id="ide1" name="env[]" value="vscode">
+          <label for="ide1"> Visual Studio Code</label><br>
+          <input type="checkbox" id="ide2" name="env[]" value="brackets">
+          <label for="ide2"> Brackets</label><br>
+          <input type="checkbox" id="ide3" name="env[]" value="other">
+          <label for="ide3"> Other</label> 
+          <input type="text" id="othername" name="othername">
+          </p>
+          <p>
+                <!-- create new button with type 'submit' for sending values of form to 'summary.php' page-->
+                <input type="submit" value="Submit" name="save" />
+          </p>
+        </form> 
+</div>
+</body>
+</html>
