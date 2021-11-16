b95ff457b98f81a7ba11e65efb8a4751e493667d
Halyna Rodriguez <loza1111@yahoo.com>
2021-11-15 20:43:16 -0500

    update

--- Lab11/courses.php
+++ Lab11/courses.php
@@ -25,7 +25,7 @@ if (!$conn) {
 }
 
 $sql = "SELECT courseid, title, description FROM it1150 ORDER BY lastname";
-$result = $conn->query($sql);
+$result = mysqli_query($conn, $sql);
 
 ?>
 <table>
@@ -44,8 +44,8 @@ $result = $conn->query($sql);
 </tr>
 <tr><th>Course Id</th><th>Title</th><th>Credit Hrs</th><th>Description</th></tr>
 <?php
-if ($result->num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
+if (mysqli_num_rows($result) > 0) {
+  while($row = mysqli_fetch_assoc($result)) {
     echo "<tr><td>" . $row["course_id"]. "</td><td>" . $row["title"]. "</td>" . 
          "<td>" . $row["credit_hrs"]. "</td><td>" . $row["description"]. "</td></tr>";
   }
